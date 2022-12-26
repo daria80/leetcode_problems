0731ce7870b925e1707f7561917d2a99595db7a8
smirnova <dddsssmmm555@mail.ru>
2022-12-26 12:15:32 +0300

    add new task

--- src/com/company/Longest_Common_Prefix.java
+++ src/com/company/Longest_Common_Prefix.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class Longest_Common_Prefix {
+    public static void main(String[] args) {
+        String[] strs = {"abcde", "abcde", "abcde"};
+
+        lcp(strs);
+        System.out.println(lcp(strs));
+    }
+
+    public static String lcp(String[] strs) {
+        if(strs == null || strs.length == 0) return "";
+        String s = strs[0];
+
+        for(int i = 0; i < strs.length; i++) {
+            while(strs[i].indexOf(s) != 0) {
+                s = s.substring(0,s.length() - 1);
+                if(s.isEmpty()) return "";
+            }
+        }
+        return s;
+    }
 }
